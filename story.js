c3b46a8d1b680a5835c8c6c80010d2c6f95268f0
gabrielasantana05 <gabriela.santana6051790@gmail.com>
2026-05-21 08:01:51 -0400

    commit

---
+++ story.js
@@ -0,0 +1,7 @@
+let name = prompt("Main character's name:");
+let friend = prompt("Friend's name:");
+let town = prompt("Town name:");
+let snack = prompt("Favorite snack:");
+let drink = prompt("Favorite drink:");
+let place = prompt("Summer place:");
+let activity = prompt("Fun activity:");
