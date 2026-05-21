b46d65e43b6114071c14277cb95cd679a5f5badd
esmeralda9th <esmeraldajosecruzh2@gmail.com>
2026-05-21 08:23:54 -0400

    commit

--- story.js
+++ story.js
@@ -4,4 +4,26 @@ let town = prompt("Town name:");
 let snack = prompt("Favorite snack:");
 let drink = prompt("Favorite drink:");
 let place = prompt("Summer place:");
-let activity = prompt("Fun activity:");
+let pet = prompt("Type of pet:");
+let petName = prompt("Pet's name:");
+let weather = prompt("Summer weather:");
+let object = prompt("Something they find:");
+let vehicle = prompt("Something they ride:");
+let music = prompt("Type of music:");
+let color = prompt("Favorite color:");
+let time = prompt("Time of day:");
+
+let story =
+"It was a " + weather + " summer afternoon in " + town + ". " +
+
+"At around " + time + ", " + name +
+" met up with their friend " + friend +
+" at the " + place + ". " +
+
+name + " brought their " + pet +
+" named " + petName +
+", who followed them everywhere. " +
+
+"While listening to " + music +
+" music, they shared some " + snack +
+" and drank cold " + drink + ". " +
